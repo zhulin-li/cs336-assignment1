4ad9251759873c72b6d0ecdb0c1f9ab489e38be4
Zhulin Li <5005887+zhulin-li@users.noreply.github.com>
2025-08-17 21:34:22 -0400

    tokenizer opt

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,20 +1,28 @@
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Callable
 import pickle
 from cs336_basics.bpe import ENCODING, PAT
 import regex as re
 
 
 class Token:
-    def __init__(self, data: bytes, id: int, next=None):
-        """
-        vocab[id] = data
-        """
+    def __init__(self, data: bytes, next=None):
         self.data = data
-        self.id = id
         self.next: Token = next
 
     def __repr__(self) -> str:
         return f"Token({self.data=})"
+
+
+class MemoizeDict(dict):
+    def __init__(self, func: Callable):
+        super().__init__()
+        assert callable(func)
+        self.func = func
+
+    def __missing__(self, key):
+        result = self.func(key)
+        self[key] = result
+        return result
 
 
 class Tokenizer:
@@ -25,16 +33,18 @@ class Tokenizer:
         special_tokens: list[str] | None = None,
     ):
         self.vocab = vocab
+        self.vocab_reverse = {token: i for i, token in vocab.items()}
         self.merges = merges
+
         if special_tokens:
             self.special_tokens = sorted(special_tokens, reverse=True)
         else:
             self.special_tokens = None
-
         self.special_token_to_id = self._compute_special_token_to_id()
-        self.single_byte_to_id = self._compute_single_byte_to_id()
-        self.vocab_merge_offset = self._compute_vocab_merge_offset()
-        self._validate_vocab_merge_consistency()
+
+        self.words2ids_cache: MemoizeDict[bytes, list[int]] = MemoizeDict(
+            func=self.word2ids
+        )
 
     def _compute_special_token_to_id(self) -> dict[str, int]:
         special_token_to_id: dict[str, int] = dict()
@@ -55,30 +65,6 @@ class Tokenizer:
         assert len(special_token_to_id) == len(self.special_tokens)
         return special_token_to_id
 
-    def _compute_single_byte_to_id(self) -> dict[bytes, int]:
-        single_byte_to_id: dict[bytes, int] = dict()
-        for i in range(2**8):
-            single_byte = self.vocab[i]
-            assert len(single_byte) == 1
-            single_byte_to_id[single_byte] = i
-        return single_byte_to_id
-
-    def _compute_vocab_merge_offset(self) -> int:
-        """
-        We assume that during BPE, once merge starts, the vocab
-        and merges will be updated in lockstep.
-        """
-        first_merge_data = b"".join(self.merges[0])
-        for i_vocab in range(len(self.vocab)):
-            if self.vocab[i_vocab] == first_merge_data:
-                return i_vocab
-        raise RuntimeError("Did not find vocab_merge_offset!")
-
-    def _validate_vocab_merge_consistency(self) -> None:
-        for i_merge, merge in enumerate(self.merges):
-            i_vocab = self.vocab_merge_offset + i_merge
-            assert self.vocab[i_vocab] == b"".join(merge)
-
     @classmethod
     def from_files(
         cls,
@@ -96,41 +82,30 @@ class Tokenizer:
         prev: Token = None
         first_token: Token = None
         for i in word:
-            data = bytes([i])
-            token = Token(data, id=self.single_byte_to_id[data])
+            token = Token(data=bytes([i]))
             if prev:
                 prev.next = token
             else:
                 first_token = token
             prev = token
 
-        for i_merge, merge in enumerate(self.merges):
-            i_vocab = i_merge + self.vocab_merge_offset
+        for merge in self.merges:
             token = first_token
-
-            is_first_token = True
             while token and token.next:
                 if (token.data, token.next.data) == merge:
                     token.data = token.data + token.next.data
-                    token.id = i_vocab
-                    assert self.vocab[i_vocab] == b"".join(merge)
                     token.next = token.next.next
-                if is_first_token:
-                    first_token = token
                 token = token.next
-                is_first_token = False
 
         ids = []
         token = first_token
         while token:
-            ids.append(token.id)
+            ids.append(self.vocab_reverse[token.data])
             token = token.next
 
         return ids
 
-    def encode(
-        self, text: str, *, word2ids_cache: dict[bytes, list[int]] = dict()
-    ) -> list[int]:
+    def encode(self, text: str) -> list[int]:
         """
         Encode an input text into a sequence of token IDs.
         Assume that `text` fits into memory.
@@ -150,12 +125,7 @@ class Tokenizer:
             else:
                 for match in re.finditer(PAT, chunk):
                     word: bytes = match.group().encode(encoding=ENCODING)
-                    if word in word2ids_cache:
-                        ids += word2ids_cache[word]
-                    else:
-                        x = self.word2ids(word)
-                        word2ids_cache[word] = x
-                        ids += x
+                    ids += self.words2ids_cache[word]
         return ids
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
@@ -165,18 +135,17 @@ class Tokenizer:
         memory-eﬀicient tokenization of large files that we cannot directly
         load into memory.
         """
-        word2ids_cache: dict[bytes, list[int]] = dict()
         buffer = ""
         for text in iterable:
             buffer += text
-            while len(buffer) > 1_000_000:
+            if len(buffer) > 100_000_000:
                 matches = list(re.finditer(PAT, buffer))
                 assert len(matches) > 0
                 last_match_end = matches[-1].end()
                 to_process = buffer[:last_match_end]
                 buffer = buffer[last_match_end:]
-                yield from self.encode(to_process, word2ids_cache=word2ids_cache)
-        yield from self.encode(buffer, word2ids_cache=word2ids_cache)
+                yield from self.encode(to_process)
+        yield from self.encode(buffer)
 
     def decode(self, ids: list[int]) -> str:
         """
